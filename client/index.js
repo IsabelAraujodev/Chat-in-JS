a543e8260eb10f7008295382a3931cd0907ea43a
Isabel Araújo <isabelcalvesaraujo@hotmail.com>
2023-04-05 00:51:04 -0300

    .

---
+++ client/index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+
+/*
+    chmod +x index.js
+*/
+
+/*
+npm i -g @isabel/chat-client
+npm unlink -g @isabel/chat-client
+hacker-chat \
+    --username isabel \
+    --room sala01
+./index.js \
+    --username isabel \
+    --room sala01
+node index.js \
+    --username isabel \
+    --room sala01 \
+    --hostUri localhost
+*/
+
+import Events from 'events'
+import CliConfig from './src/cliConfig.js';
+import EventManager from './src/eventManager.js';
+import SocketClient from './src/socker.js';
+import TerminalController from "./src/terminalController.js";
+
+
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+
+const componentEmitter = new Events()
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+const eventManager = new EventManager({ componentEmitter, socketClient})
+const events = eventManager.getEvents()
+socketClient.attachEvents(events)
+
+const data = {
+    roomId: config.room,
+    userName: config.username
+}
+eventManager.joinRoomAndWaitForMessages(data)
+
+const controller = new TerminalController()
+await controller.initializeTable(componentEmitter)
